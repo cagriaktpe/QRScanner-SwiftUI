d0d27332cc57a685858b1674c4bba566cf495802
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-03-18 14:05:00 +0300

    Initial Commit

---
+++ QRScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRScanner
+//
+//  Created by Samet Çağrı Aktepe on 18.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Samet Çağrı Aktepe on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
