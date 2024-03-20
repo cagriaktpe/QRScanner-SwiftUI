c2a460ed8876540761ced716ebfdd99134e10f17
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-03-20 13:48:15 +0300

    Update Extension.swift

--- QRScanner/Extension.swift
+++ QRScanner/Extension.swift
@@ -11,7 +11,16 @@ extension String {
     // convert "20240315" to a Date
     func toDate() -> Date? {
         let dateFormatter = DateFormatter()
+        // event the time is not important, so we can ignore it
         dateFormatter.dateFormat = "yyyyMMdd"
-        return dateFormatter.date(from: self)
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            dateFormatter.dateFormat = "yyyyMMdd'T'HHmmss"
+            let date = dateFormatter.date(from: self)
+            print(date)
+            return dateFormatter.date(from: self)
+        }
+        
     }
 }
