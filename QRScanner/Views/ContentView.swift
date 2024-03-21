5c2965e56346233678931c3d817b55b7412c05ab
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-03-21 11:14:04 +0300

    Add image scanner

--- QRScanner/Views/ContentView.swift
+++ QRScanner/Views/ContentView.swift
@@ -5,40 +5,106 @@
 //  Created by Samet Çağrı Aktepe on 18.03.2024.
 //
 
-import SwiftUI
 import Contacts
 import EventKit
+import PhotosUI
+import SwiftUI
 
 struct ContentView: View {
     @Environment(\.dismiss) var dismiss
     @State var scanResult = "No QR code detected"
-    
+    @State var image: Data?
+    @State var selectedPhoto: PhotosPickerItem?
+
     // for navigations
     @State var event: IdentifiableEKEvent?
     @State var scannedContact: CNContact?
     @State var scannedText: String?
- 
+
     var body: some View {
         NavigationStack {
             ZStack(alignment: .bottom) {
                 QRScanner(result: $scanResult, scannedContact: $scannedContact, scannedEvent: $event, scannedText: $scannedText)
-   
+
+                Rectangle()
+                    .ignoresSafeArea()
+                    .foregroundStyle(.ultraThinMaterial)
+                    .frame(height: 100)
+                    .overlay {
+                        // put the overLay right corner
+                        VStack {
+                            overlayLayer
+                                .overlay {
+                                    Rectangle()
+                                        .fill(.thinMaterial)
+                                        .mask(overlayLayer)
+                                        .allowsHitTesting(false)
+                                }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing)
+                        .padding(.top)
+                    }
+
                 Text(scanResult)
+
                     .padding()
+
                     .background(.black)
+
                     .foregroundColor(.white)
+
                     .padding(.bottom)
             }
             .navigationDestination(item: $scannedText) { text in
                 ScannedTextView(text: text)
             }
-            .navigationDestination(item: $scannedContact) { contact in
+            .navigationDestination(item: $scannedContact) { _ in
                 ContactDetailView(scannedContact: $scannedContact)
             }
             .navigationDestination(item: $event) { event in
                 AddEvent(scannedEvent: $event)
             }
+            .task(id: selectedPhoto) {
+                if let data = try? await selectedPhoto?.loadTransferable(type: Data.self) {
+                    image = data
+                    detectQRCode(UIImage(data: data))?.forEach { feature in
+                        if let qrFeature = feature as? CIQRCodeFeature {
+                            scannedText = qrFeature.messageString
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    var overlayLayer: some View {
+        PhotosPicker(selection: $selectedPhoto,
+                     matching: .images,
+                     photoLibrary: .shared()) {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .frame(width: 50, height: 50)
+        }
+    }
+}
+
+extension ContentView {
+    func detectQRCode(_ image: UIImage?) -> [CIFeature]? {
+        if let image = image, let ciImage = CIImage(image: image) {
+            var options: [String: Any]
+            let context = CIContext()
+            options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+            let qrDetector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: options)
+            if ciImage.properties.keys.contains(kCGImagePropertyOrientation as String) {
+                options = [CIDetectorImageOrientation: ciImage.properties[kCGImagePropertyOrientation as String] ?? 1]
+            } else {
+                options = [CIDetectorImageOrientation: 1]
+            }
+            let features = qrDetector?.features(in: ciImage, options: options)
+            return features
+        }
+        return nil
     }
 }
 
